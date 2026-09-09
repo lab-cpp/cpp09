91629b7b023bc1d93d9853dfcb30372aafb77880
alcacere <alcacere@localhost.com>
2026-09-09 12:59:21 +0200

    updating ex01

--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -5,15 +5,12 @@
 
 class RPN {
     private:
-        // Orthodox Canonical Form (Not strictly necessary if all methods are static,
-        // but required by the module rules unless explicitly stated otherwise)
         RPN();
         RPN(const RPN& src);
         RPN& operator=(const RPN& rhs);
         ~RPN();
 
     public:
-        // Core Function
         static void evaluate(const std::string& expression);
 };
 
